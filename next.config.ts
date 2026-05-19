51106bf14ebb5014bdc47815dc041f47c93e67f9
Francis Mukaha <f.mukaha@strategine.com>
2026-05-19 21:15:45 +0300

    Resolve merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 ﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-=======
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
->>>>>>> 00cabe3960b01eec9557125a51f4d6ebaf97ece7
 };
 
 export default nextConfig;
